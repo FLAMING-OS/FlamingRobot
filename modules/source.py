ea8991d307b195b76cb83ac3b70b1f7bd9d1b757
FLAMING-OS <128779626+FLAMING-OS@users.noreply.github.com>
2023-03-24 12:23:11 +0530

    Add files via upload

---
+++ modules/source.py
@@ -0,0 +1,49 @@
+from platform import python_version as y
+
+from pyrogram import __version__ as z
+from pyrogram import filters
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+from telegram import __version__ as o
+from telethon import __version__ as s
+
+from FlamingRobot import OWNER_ID, dispatcher
+from FlamingRobot import pbot as client
+
+Flaming = "https://telegra.ph/file/3117ae688980a7163ab5e.jpg"
+
+
+@client.on_message(filters.command(["repo", "source"]))
+async def repo(client, message):
+    await message.reply_photo(
+        photo=Flaming,
+        caption=f"""**ʜᴇʏ​ {message.from_user.mention()},\n\nɪ ᴀᴍ [{dispatcher.bot.first_name}](t.me/{dispatcher.bot.username})**
+
+**» ᴍʏ ᴅᴇᴠᴇʟᴏᴘᴇʀ​ :** [PANKAJ](tg://user?id={OWNER_ID})
+**» ᴩʏᴛʜᴏɴ ᴠᴇʀsɪᴏɴ :** `{y()}`
+**» ʟɪʙʀᴀʀʏ ᴠᴇʀsɪᴏɴ :** `{o}` 
+**» ᴛᴇʟᴇᴛʜᴏɴ ᴠᴇʀsɪᴏɴ :** `{s}` 
+**» ᴘʏʀᴏɢʀᴀᴍ ᴠᴇʀsɪᴏɴ :** `{z}`
+
+**ɢʀᴏᴜᴘ ✘ ᴄᴏɴᴛʀᴏʟʟᴇʀ sᴏᴜʀᴄᴇ ɪs ɴᴏᴡ ᴩᴜʙʟɪᴄ ᴀɴᴅ ɴᴏᴡ ʏᴏᴜ ᴄᴀɴ ᴍᴀᴋᴇ ʏᴏᴜʀ ᴏᴡɴ ʙᴏᴛ.**
+""",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "• ᴏᴡɴᴇʀ ",f"tg://user?id={OWNER_ID}"
+                    ),
+                    InlineKeyboardButton(
+                        "• ʀᴇᴘᴏ •",
+                        url="https://github.com/FLAMING-OS/FlamingRobot",
+                    ),
+                ]
+            ]
+        ),
+    )
+
+
+__mod_name__ = "⍟ Rᴇᴩᴏ ⍟"
+_help__ = """
+ /repo  ᴛᴏ ɢᴇᴛ ʀᴇᴘᴏ 
+ /source ᴛᴏ ɢᴇᴛ ʀᴇᴘᴏ
+"""
